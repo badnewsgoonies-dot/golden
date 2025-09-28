81625c3efa80fe326f1b4d2d34ec96742c4e88af
jim jam <badnewsgoonies@gmail.com>
2025-09-28 18:27:41 -0400

    Update CommandMenu.gd with new content

--- ui/CommandMenu.gd
+++ ui/CommandMenu.gd
@@ -1,8 +1,5 @@
 class_name CommandMenu
+extends Control
 
-# CommandMenu.gd
-
-# The content of this file has been updated to fix the add_spacer call.
-
-# Updated code snippet
-_header.add_spacer(false)
+func _init():
+    pass
