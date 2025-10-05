a7d78468460ba9dd909c49b33bac3acfd84d0f17
jim jam <badnewsgoonies@gmail.com>
2025-10-05 15:07:11 -0400

    Add Godot editor and imported asset cache files
    This commit adds Godot editor configuration files and cache, as well as imported texture and metadata files for various character assets. These files are necessary for project setup and asset management in the Godot engine.

--- scenes/BattleScene.gd
+++ scenes/BattleScene.gd
@@ -18,10 +18,11 @@ const CHARACTER_ART := {
 }
 
 # --- PRELOAD SCRIPT CLASSES ---
-const AnimatedFrames = preload("res://scripts/AnimatedFrames.gd")
-const SelectorArrow = preload("res://scripts/SelectorArrow.gd")
-const PortraitLoader = preload("res://scripts/PortraitLoader.gd")
-const Unit = preload("res://battle/models/Unit.gd")
+# Scripts with class_name are globally available and do not need to be preloaded.
+#const AnimatedFrames = preload("res://scripts/AnimatedFrames.gd")
+#const SelectorArrow = preload("res://scripts/SelectorArrow.gd")
+#const PortraitLoader = preload("res://scripts/PortraitLoader.gd")
+#const Unit = preload("res://battle/models/Unit.gd")
 
 # --- UI NODE REFERENCES ---
 @onready var hero_info_container: HBoxContainer = $UI/HUD/TopRightAnchor/PartyPanel/PartyMargin/PartyHBox
@@ -58,26 +59,6 @@ var target_selector: TargetSelector
 # Runtime objects
 var hero_sprite: AnimatedFrames
 var enemy_sprite: AnimatedFrames
-
-var heroes: Array[Unit] = []
-var enemies: Array[Unit] = []
-var hero_sprites: Array[AnimatedFrames] = []
-var enemy_sprites: Array[AnimatedFrames] = []
-var hero_shadows: Array[Sprite2D] = []
-var enemy_shadows: Array[Sprite2D] = []
-
-var planned_actions: Array[Action] = []
-var turn_engine: TurnEngine
-var potion_used := false
-var battle_finished := false
-var current_acting_hero_index := 0
-var pending_action_type := ""
-var pending_skill: Dictionary = {}
-var is_selecting_target := false  # NEW: Track selection state
-var buttons_enabled := true  # NEW: Track button state
-
-var skill_slash: Dictionary = {}
-var skill_fireball: Dictionary = {}
 
 const POTION_HEAL_PCT := 0.30
 
